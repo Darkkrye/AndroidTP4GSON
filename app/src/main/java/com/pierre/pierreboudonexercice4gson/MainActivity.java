fc2c708f6d8f61c1d8fe728ab400c9e07081a606
florent champigny <fchampigny@xebia.fr>
2016-05-23 13:24:37 +0200

    correction

--- app/src/main/java/com/pierre/pierreboudonexercice4gson/MainActivity.java
+++ app/src/main/java/com/pierre/pierreboudonexercice4gson/MainActivity.java
@@ -35,10 +35,16 @@ public class MainActivity extends AppCompatActivity {
     TextView public_reposTV;
     TextView created_atTV;
 
+    OkHttpClient okhttpClient;
+    Gson gson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.okhttpClient = new OkHttpClient(); //CORRECTION enhancement : only 1 okhttp
+        this.gson = new Gson(); //CORRECTION enhancement :  only 1 gson
 
         loginTV = (TextView)findViewById(R.id.loginTextView);
         idTV = (TextView)findViewById(R.id.idTextView);
@@ -58,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onClick(View v) {
-                    OkHttpClient okhttpClient = new OkHttpClient();
                     Request myGetRequest = new Request.Builder()
                             .url("https://api.github.com/users/" + githubUser.getText())
                             .build();
@@ -69,14 +74,13 @@ public class MainActivity extends AppCompatActivity {
                             final String text = response.body().string();
                             final int statusCode = response.code();
 
-                            Gson gson = new Gson();
                             final User user = gson.fromJson(text, User.class);
 
                                 /*MainActivity.this.*/runOnUiThread(new Runnable() {
                                 public void run() {
                                     loginTV.setText("Login: " + user.getLogin());
                                     idTV.setText("ID: " + user.getId());
-                                    avatar_urlTV.setText("Avatar URL: " + user.getAvatar_url());
+                                    avatar_urlTV.setText("Avatar URL: " + user.getAvatarUrl());
                                     urlTV.setText("URL: " + user.getUrl());
                                     html_urlTV.setText("HTML URL: " + user.getHtml_url());
                                     gists_urlTV.setText("Gists URL: " + user.getGists_url());
